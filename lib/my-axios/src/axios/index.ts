00b7961d5d2b1a43ee63216cbeb237186cc9d552
wanghan <w_han123@163.com>
2023-03-06 19:48:14 +0800

    axios

--- lib/my-axios/src/axios/index.ts
+++ lib/my-axios/src/axios/index.ts
@@ -1,6 +1,39 @@
+interface RequestParams {
+    url: string;
+    method: 'get' | 'post' | 'delete' | 'put' | 'options'
+}
+
 class Axios {
+    constructor() {
+        const defaultOptions:any = {
+            baseURL: '',
+            timeout: 100,
+            headers: {}
+        }
+        this.default = defaultOptions;
+    }
     static create() {
         return new Axios()
+    }
+    get() {
+
+    }
+    post() {
+
+    }
+    request(params: RequestParams) {
+        const {
+            url,
+            method
+        } = params;
+        const xhr = new XMLHttpRequest()
+        xhr.open(method.toUpperCase(), url, false)
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                const res = xhr.responseText
+            }
+        }
+        xhr.send()
     }
 }
 
