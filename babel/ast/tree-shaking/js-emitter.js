f366a481c67ef230807f5c95ba345e9553e8c03a
wanghan <1594078165@qq.com>
2023-08-06 20:37:46 +0800

    tree-shaking

--- babel/ast/tree-shaking/js-emitter.js
+++ babel/ast/tree-shaking/js-emitter.js
@@ -1,6 +1,70 @@
 class JSEmitter {
     constructor() {
     }
+    visitVariableDeclaration(node) {
+        let str = '';
+        str += node.kind + ' ';
+        str += this.visitNodes(node.declarations);
+        return str + '\n';
+    }
+    visitVariableDeclarator(node, kind) {
+        let str = kind ? (kind + ' ') : '';
+        str += this.visitNode(node.id);
+        str += '=';
+        str += this.visitNode(node.init);
+        return str + ';' + '\n';
+    }
+    visitLiteral(node) {
+        return node.raw;
+    }
+    visitIdentifier(node) {
+        return node.name;
+    }
+    visitBinaryExpression(node) {
+        let str = '';
+        str += this.visitNode(node.left);
+        str += node.operator;
+        str += this.visitNode(node.right);
+        return str + '\n';
+    }
+    visitFunctionDeclaration(node) {
+        let str = 'function ';
+        str += this.visitNode(node.id);
+        str += '(';
+        for (let paramIndex = 0; paramIndex < node.params.length; paramIndex++) {
+            str += this.visitNode(node.params[paramIndex]);
+            str += ((node.params[paramIndex] === undefined) ? '' : ',')
+        }
+        str = str.slice(0, str.length - 1);
+        str += '){\n';
+        str += this.visitNode(node.body);
+        str += '}';
+        return str + '\n';
+    }
+    visitBlockStatement(node) {
+        let str = '';
+        str += this.visitNodes(node.body);
+        return str;
+    }
+    visitCallExpression(node) {
+        let str = '';
+        str += this.visitIdentifier(node.callee);
+        str += '(';
+        for (const arg of node.arguments) {
+            str += this.visitNode(arg) + ',';
+        }
+        str = str.slice(0, -1);
+        str += ');';
+        return str + '\n';
+    }
+    visitReturnStatement(node) {
+        let str = '';
+        str = str + '  return ' + this.visitNode(node.argument);
+        return str + '\n';
+    }
+    visitExpressionStatement(node) {
+        return this.visitNode(node.expression);
+    }
     visitNodes(nodes) {
         let str = '';
         for (const node of nodes) {
@@ -10,6 +74,39 @@ class JSEmitter {
     }
     visitNode(node) {
         let str = '';
+        switch (node.type) {
+            case 'VariableDeclaration':
+                str += this.visitVariableDeclaration(node);
+                break;
+            case 'VariableDeclarator':
+                str += this.visitVariableDeclarator(node);
+                break;
+            case 'Literal':
+                str += this.visitLiteral(node);
+                break;
+            case 'Identifier':
+                str += this.visitIdentifier(node);
+                break;
+            case 'BinaryExpression':
+                str += this.visitBinaryExpression(node);
+                break;
+            case 'FunctionDeclaration':
+                str += this.visitFunctionDeclaration(node);
+                break;
+            case 'BlockStatement':
+                str += this.visitBlockStatement(node);
+                break;
+            case 'CallExpression':
+                str += this.visitCallExpression(node);
+                break;
+            case 'ReturnStatement':
+                str += this.visitReturnStatement(node);
+                break;
+            case 'ExpressionStatement':
+                str += this.visitExpressionStatement(node);
+                break;
+        }
+        return str;
     }
     run(body) {
         let str = '';
--- babel/ast/tree-shaking/tree-shaking.js
+++ babel/ast/tree-shaking/tree-shaking.js
@@ -1,13 +1,16 @@
 const acorn = require('acorn');
 const fs = require('fs');
 const JSEmitter = require('./js-emitter');
-const args = process.args[2];
-const buffer = fs.readFileSync(args).toString();
-const body = acorn.parse(buffer).body;
+const path = require('path');
+// const args = process.args[2];
+const buffer = fs.readFileSync(path.resolve(__dirname, './test.js')).toString();
+const body = acorn.parse(buffer, {
+    ecmaVersion: 'latest',
+}).body;
 const jsEmitter = new JSEmitter();
 const decls = new Map();
 const calledDecls = [];
-const code = []
+let code = []
 
 body.forEach(node => {
     if (node.type === 'FunctionDeclaration') {
@@ -15,4 +18,30 @@ body.forEach(node => {
         decls.set(jsEmitter.visitNode(node.id), code);
         return;
     }
+    if (node.type === 'VariableDeclaration') {
+        for (const decl of node.declarations) {
+            decls.set(jsEmitter.visitNode(decl.id), jsEmitter.visitVariableDeclarator(decl, node.kind));
+        }
+        return;
+    }
+    if (node.type === 'ExpressionStatement') {
+        debugger;
+        if (node.expression.type === 'CallExpression') {
+            const callNode = node.expression;
+            calledDecls.push(jsEmitter.visitIdentifier(callNode.callee));
+            for (const arg of callNode.arguments) {
+                if (arg.type === 'Identifier') {
+                    calledDecls.push(arg.name);
+                }
+            }
+        }
+    }
+    if (node.type === 'Identifier') {
+        calledDecls.push(node.name);
+    }
+    code.push(jsEmitter.run([node]));
 })
+code = calledDecls.map(c => decls.get(c)).concat(code).join('');
+// console.log('decls', decls);
+// console.log('calledDecls', calledDecls);
+console.log(code);
